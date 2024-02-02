e8b574fa672a0de0d68ddc23714f189329df7bd1
MRuzhevich <mazaahakaa@gmail.com>
2024-02-02 12:08:19 +0300

    feat: added conditions

--- Calculator.cpp
+++ Calculator.cpp
@@ -8,7 +8,7 @@ int main()
 	int n = 1;
 	double Del, Energy, Wave;
 	char yn = 'y';
-
+	
 	do {
 
 		cout << "Choose opton:" << endl;
@@ -20,43 +20,53 @@ int main()
 		cout << "6. Wave -> Del" << endl;
 
 		cin >> number;
-
-		switch (number) {
-		case 1:
-			cout << "Enter Del (mm): " << endl;
-			cin >> Del;
-			cout << "Energy " << 1240 / (Del * 8) << " eV" << endl;
-			break;
-		case 2:
-			cout << "Enter Energy (eV): " << endl;
-			cin >> Energy;
-			cout << "Del " << 1240 / (Energy * 8) << " mm" << endl;
-			break;
-		case 3:
-			cout << "Enter Wave (um): " << endl;
-			cin >> Wave;
-			cout << "Energy " << 1240 / Wave << " eV" << endl;
-			break;
-		case 4:
-			cout << "Enter Energy (eV): " << endl;
-			cin >> Energy;
-			cout << "Wave " << 1240 / Energy << " um" << endl;
-			break;
-		case 5:
-			cout << "Enter Del (mm): " << endl;
-			cin >> Del;
-			cout << "Wave " << Del * 8 << " um" << endl;
-			break;
-		case 6:
-			cout << "Enter Wave (um): " << endl;
-			cin >> Wave;
-			cout << "Del " << Wave / 8 << " mm" << endl;
-			break;
+		if (typeid(number).name() == "int") {
+			if (number >= 1 && number <= 6) {
+				switch (number) {
+				case 1:
+					cout << "Enter Del (mm): " << endl;
+					cin >> Del;
+					cout << "Energy " << 1240 / (Del * 8) << " eV" << endl;
+					break;
+				case 2:
+					cout << "Enter Energy (eV): " << endl;
+					cin >> Energy;
+					cout << "Del " << 1240 / (Energy * 8) << " mm" << endl;
+					break;
+				case 3:
+					cout << "Enter Wave (um): " << endl;
+					cin >> Wave;
+					cout << "Energy " << 1240 / Wave << " eV" << endl;
+					break;
+				case 4:
+					cout << "Enter Energy (eV): " << endl;
+					cin >> Energy;
+					cout << "Wave " << 1240 / Energy << " um" << endl;
+					break;
+				case 5:
+					cout << "Enter Del (mm): " << endl;
+					cin >> Del;
+					cout << "Wave " << Del * 8 << " um" << endl;
+					break;
+				case 6:
+					cout << "Enter Wave (um): " << endl;
+					cin >> Wave;
+					cout << "Del " << Wave / 8 << " mm" << endl;
+					break;
+				}
+				cout << "Continue? Y/N" << endl;
+				cin >> yn;
+			}
+			else {
+				cout << "Enter number from 1 to 6" << endl;
+			}
 		}
-cout << "Continue? Y/N" << endl;
-cin >> yn;
+		else {
+			cout << "Enter number" << endl;
+			continue;
+		}
 	} while (yn == 'y');
-		
+
 
 
 	system("pause>0");
