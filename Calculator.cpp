fc4a2f44faa5d34daa6ab06d80062f8f9340933a
MRuzhevich <mazaahakaa@gmail.com>
2024-02-04 01:57:58 +0300

    feat: new logic for input

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
 void ShowMenu() {
-	cout << "Choose opton:" << endl;
+	cout << "Choose option:" << endl;
 	cout << "1. Del -> Energy" << endl;
 	cout << "2. Energy -> Del" << endl;
 	cout << "3. Wave -> Energy" << endl;
@@ -15,72 +16,79 @@ void ShowMenu() {
 
 int main()
 {
-	int number;
-	int n = 1;
+	int intinput;
+
 	double Del, Energy, Wave;
-	char yn = 'y';
-	char m = 'M';
+
+	string input;
 
 	ShowMenu();
 
-	do {
+	while (1) {
 
-		if (cin >> m) {
-			ShowMenu();
-		}
+		cin.sync();
 
-		cin >> number;
+		getline(cin, input);
 
-		if (typeid(number) == typeid(int&)) {
-			if (number >= 1 && number <= 6) {
-				switch (number) {
-				case 1:
-					cout << "Enter Del (mm): " << endl;
-					cin >> Del;
-					cout << "Energy " << 1240 / (Del * 8) << " eV" << endl;
-					break;
-				case 2:
-					cout << "Enter Energy (eV): " << endl;
-					cin >> Energy;
-					cout << "Del " << 1240 / (Energy * 8) << " mm" << endl;
-					break;
-				case 3:
-					cout << "Enter Wave (um): " << endl;
-					cin >> Wave;
-					cout << "Energy " << 1240 / Wave << " eV" << endl;
-					break;
-				case 4:
-					cout << "Enter Energy (eV): " << endl;
-					cin >> Energy;
-					cout << "Wave " << 1240 / Energy << " um" << endl;
-					break;
-				case 5:
-					cout << "Enter Del (mm): " << endl;
-					cin >> Del;
-					cout << "Wave " << Del * 8 << " um" << endl;
-					break;
-				case 6:
-					cout << "Enter Wave (um): " << endl;
-					cin >> Wave;
-					cout << "Del " << Wave / 8 << " mm" << endl;
-					break;
-				}
-				cout << "Continue? Y/N" << endl;
-				cin >> yn;
-			}
-			else {
-				cout << "Enter number from 1 to 6" << endl;
-			}
-		}
-		else {
+		if (input == "y" || input == "Y") {
 			cout << "Enter number" << endl;
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+		if (input == "n" || input == "N") {
 			break;
 		}
-	} while (yn == 'y' && cin >> number);
 
+		if (input == "M") {
+			ShowMenu();
+			continue;
+		}
+		try {
+			intinput = stoi(input);
+		}
+		catch (invalid_argument const& ex) {
+			cout << "Error " << ex.what() << '\n';
+			cout << "Enter number" << '\n';
+			continue;
+		}
 
-
-	cin.get();
+		if (intinput >= 1 && intinput <= 6) {
+			switch (intinput) {
+			case 1:
+				cout << "Enter Del (mm): " << endl;
+				cin >> Del;
+				cout << "Energy " << 1240 / (Del * 8) << " eV" << endl;
+				break;
+			case 2:
+				cout << "Enter Energy (eV): " << endl;
+				cin >> Energy;
+				cout << "Del " << 1240 / (Energy * 8) << " mm" << endl;
+				break;
+			case 3:
+				cout << "Enter Wave (um): " << endl;
+				cin >> Wave;
+				cout << "Energy " << 1240 / Wave << " eV" << endl;
+				break;
+			case 4:
+				cout << "Enter Energy (eV): " << endl;
+				cin >> Energy;
+				cout << "Wave " << 1240 / Energy << " um" << endl;
+				break;
+			case 5:
+				cout << "Enter Del (mm): " << endl;
+				cin >> Del;
+				cout << "Wave " << Del * 8 << " um" << endl;
+				break;
+			case 6:
+				cout << "Enter Wave (um): " << endl;
+				cin >> Wave;
+				cout << "Del " << Wave / 8 << " mm" << endl;
+				break;
+			}
+			cout << "Continue? Y/n" << endl;
+		}
+		else {
+			cout << "Enter number from 1 to 6" << endl;
+		}
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
 }
