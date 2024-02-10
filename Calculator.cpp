62c13c0f3e7ce22c777450213f32f56ab754d03b
MRuzhevich <mazaahakaa@gmail.com>
2024-02-10 17:12:14 +0300

    feat: switch to 3 convert options

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 void ShowMenu() {
 	cout << "Choose option:" << endl;
-	cout << "1. Del -> Energy" << endl;
-	cout << "2. Energy -> Del" << endl;
-	cout << "3. Wave -> Energy" << endl;
-	cout << "4. Energy -> Wave" << endl;
-	cout << "5. Del -> Wave" << endl;
-	cout << "6. Wave -> Del" << endl;
+	cout << "1. Del -> Energy and Del -> Wave" << endl;
+	cout << "2. Energy -> Del and Energy -> Wave" << endl;
+	cout << "3. Wave -> Energy and Wave -> Del" << endl;
 	cout << "Type M to show menu" << endl;
 }
 
@@ -51,43 +48,31 @@ int main()
 			continue;
 		}
 
-		if (intinput >= 1 && intinput <= 6) {
+		if (intinput >= 1 && intinput <= 3) {
 			switch (intinput) {
 			case 1:
 				cout << "Enter Del (mm): " << endl;
 				cin >> Del;
 				cout << "Energy " << 1240 / (Del * 8) << " eV" << endl;
+				cout << "Wave " << Del * 8 << " um" << endl;
 				break;
 			case 2:
 				cout << "Enter Energy (eV): " << endl;
 				cin >> Energy;
 				cout << "Del " << 1240 / (Energy * 8) << " mm" << endl;
+				cout << "Wave " << 1240 / Energy << " um" << endl;
 				break;
 			case 3:
 				cout << "Enter Wave (um): " << endl;
 				cin >> Wave;
 				cout << "Energy " << 1240 / Wave << " eV" << endl;
-				break;
-			case 4:
-				cout << "Enter Energy (eV): " << endl;
-				cin >> Energy;
-				cout << "Wave " << 1240 / Energy << " um" << endl;
-				break;
-			case 5:
-				cout << "Enter Del (mm): " << endl;
-				cin >> Del;
-				cout << "Wave " << Del * 8 << " um" << endl;
-				break;
-			case 6:
-				cout << "Enter Wave (um): " << endl;
-				cin >> Wave;
 				cout << "Del " << Wave / 8 << " mm" << endl;
 				break;
 			}
 			cout << "Continue? Y/n" << endl;
 		}
 		else {
-			cout << "Enter number from 1 to 6" << endl;
+			cout << "Enter number from 1 to 3" << endl;
 		}
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	}
